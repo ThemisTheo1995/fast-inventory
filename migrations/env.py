9d4511e4c150a7d683af0a9e100ccf482a2e7486
ThemisTheo1995 <themistheodoratos@gmail.com>
2026-09-05 23:50:04 +0100

    Remove parser

--- migrations/env.py
+++ migrations/env.py
@@ -14,8 +14,7 @@ settings = get_settings()
 current_url = config.get_main_option("sqlalchemy.url")
 
 if not current_url or current_url.startswith("driver://"):
-    safe_url = str(settings.DATABASE_URL).replace("%", "%%")
-    config.set_main_option("sqlalchemy.url", safe_url)
+    config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URL))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
