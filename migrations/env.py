6a51fdf8343bca2f9768e57a6791821604db3e4e
ThemisTheo1995 <themistheodoratos@gmail.com>
2026-09-05 23:20:31 +0100

    Another try of pushing migrations

--- migrations/env.py
+++ migrations/env.py
@@ -14,7 +14,8 @@ settings = get_settings()
 current_url = config.get_main_option("sqlalchemy.url")
 
 if not current_url or current_url.startswith("driver://"):
-    config.set_main_option("sqlalchemy.url", str(settings.DATABASE_URL))
+    safe_url = str(settings.DATABASE_URL).replace("%", "%%")
+    config.set_main_option("sqlalchemy.url", safe_url)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
