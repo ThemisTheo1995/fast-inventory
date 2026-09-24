55d2de8e50c7bddb6a347913e04ee053711eeeec
ThemisTheo1995 <themistheodoratos@gmail.com>
2026-09-24 18:07:55 +0100

    .

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -21,9 +21,9 @@ os.environ["DATABASE_URL"] = os.environ["TEST_DATABASE_URL"]
 
 os.environ.setdefault("AUTH_SECRET_KEY", "testing")
 os.environ.setdefault("AUTH_ALGORITHM", "TESTALGO")
-os.environ.setdefault("AUTH_ACCESS_TOKEN_EXPIRE_MINUTES", 5)
-os.environ.setdefault("AUTH_REFRESH_TOKEN_EXPIRE_DAYS", 7)
-os.environ.setdefault("COOKIE_SECURE", 7)
+os.environ.setdefault("AUTH_ACCESS_TOKEN_EXPIRE_MINUTES", "5")
+os.environ.setdefault("AUTH_REFRESH_TOKEN_EXPIRE_DAYS", "7")
+os.environ.setdefault("COOKIE_SECURE", "1")
 
 os.environ.setdefault("AWS_ACCESS_KEY_ID", "testing")
 os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "testing")
